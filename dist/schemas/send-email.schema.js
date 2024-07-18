ebc88c199231d6270840f9363c0b9e2b64570fbb
“Jack <jack28jaramillo@gmail.com>
2024-07-18 15:48:43 -0500

    vercel update gitignore

--- dist/schemas/send-email.schema.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-export const sendEmailSchema = z.object({
-    names: z.string()
-        .min(3, 'El nombre debe tener al menos 3 caracteres'),
-    email: z.string()
-        .email('El correo electrónico no es válido'),
-    phone: z.string()
-        .regex(/^(\+\d+ )?(\(\d+( \d+)*\) )?\d+( \d+)*$/, 'El número de teléfono no es válido')
-});
