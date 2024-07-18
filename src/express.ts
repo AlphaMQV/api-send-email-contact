70464f52bda589a626fd0a306fd044205bdf67d2
“Jack <jack28jaramillo@gmail.com>
2024-07-18 14:35:18 -0500

    update cors configuration

--- src/express.ts
+++ src/express.ts
@@ -1,29 +1,32 @@
-import cors from 'cors'
+import cors, { type CorsOptions } from 'cors'
 import express from 'express'
 import { sendRouter } from './routes/send.js'
 
-export const appExpress = express()
+const appExpress = express()
 
 appExpress.disable('x-powered-by')
 
-// const ACCEPTED_ORIGINS = ['https://landing-page-huaraz.vercel.app']
+const ACCEPTED_ORIGINS = ['https://landing-page-huaraz.vercel.app']
 
-// const corsOptions: CorsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin || ACCEPTED_ORIGINS.includes(origin)) {
-//       callback(null, true)
-//       return
-//     }
-//     callback(new Error('Not allowed by CORS'))
-//   },
-//   optionsSuccessStatus: 200
-// }
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || ACCEPTED_ORIGINS.includes(origin)) {
+      callback(null, true)
+      return
+    }
+    callback(new Error('Not allowed by CORS'))
+  },
+  optionsSuccessStatus: 200
+}
 
 appExpress.use(express.json())
-appExpress.use(cors())
+appExpress.use(cors(corsOptions))
 
 appExpress.use('/send', sendRouter)
 
 appExpress.use((_, res) => {
   res.status(404).json({ error: 'Ruta no encontrada' })
 })
+
+export { appExpress }
+
